6dc02a30e71dc2fb8e65e4d216e26459ac44e1a4
Hatim <rodanddragon@gmail.com>
2020-07-31 07:39:44 -0700

    Added the Python Function

--- src/api/student/student.controller.js
+++ src/api/student/student.controller.js
@@ -3,7 +3,49 @@ const {spawn} = require('child_process');
 // const connection = require('../../config/db');
 const db=require('../../config/cloudantdb');
 const { uuid } = require('uuidv4');
-/*
+
+//Function-For-PythonCalling
+
+function pythonFunction (value){
+    //Here-python-worked-for-me-instead-of-python3
+  return new Promise((resolve,reject)=>{
+
+    const python = spawn('python', ['./web_plagiarism_check.py',value]);
+    // collect data from script
+    console.log("In-function-report");
+    var report;
+    python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        report = data.toString();
+        console.log(report);
+        console.log(value);
+        let url =report.substring(report.indexOf("u")+1,report.indexOf(")")).replace("'",'');
+        let percent=report.substring(1,report.indexOf(","));
+        var plagiarismReport={
+            "percentageCopied":percent,
+            "urlUsed":url
+        }
+        resolve(plagiarismReport)
+    });
+    
+    // in close event we are sure that stream from child process is closed
+   
+  })
+
+}
+
+let getPlagReport = async(content) => {
+            var list=[]
+            for(var item of content){
+                if(item.isSubjective){
+                    var getValue = await pythonFunction(item.answer);
+                    list.push(getValue)
+                }
+            }
+            return(list);
+}
+
+
 module.exports = {
     getAll: async () => {
         return new Promise((resolve, reject) => {
@@ -97,47 +139,23 @@ module.exports = {
                                 list.plagiarism_report=[];
 
                                 // Calling the python function for the Each Subjective-Answer
-                                test_content.forEach((items,index)=>{
-                                    if(items.isSubjective){
-                                        // Calling python function
-                                        const python = spawn('python', ['./web_plagiarism_check.py',items.answer]);
-                                        // collect data from script
-                                        var report;
-                                        python.stdout.on('data', function (data) {
-                                            console.log('Pipe data from python script ...');
-                                            report = data.toString();
-                                            // console.log(report);
-                                        });
+                                getPlagReport(test_content).then((plaglist)=>{
+                                    list.plagiarism_report=plaglist;
 
-                                        // // in close event we are sure that stream from child process is closed
-                                        python.on('close', (code) => {
-                                        console.log(`child process close all stdio with code ${code}`);
-                                        let url =report.substring(report.indexOf("u")+1,report.indexOf(")")).replace("'",'');
-                                        let percent=report.substring(1,report.indexOf(","));
-                                        // console.log(url);
-                                        // console.log(percent);
-                                        var plagiarismReport={
-                                            "percentageCopied":percent,
-                                            "urlUsed":url
+                                    db.insert(list, (err, result) => {
+                                        if (err) {
+                                            logger.error('Error occurred: ' + err.message, 'create()');
+                                            reject(err);
+                                        } else {
+                                            resolve({ data: { createdId: result.id, createdRevId: result.rev }, statusCode: 201 });
+                                            console.log(result);
                                         }
-                                        list.plagiarism_report.push(plagiarismReport)
-                                        console.log(list.plagiarism_report);
-                                        // res.send(report)
-                                        });                                       
+                                    });
 
-                                    }
-                                });
 
-                                db.insert(list, (err, result) => {
-                                    if (err) {
-                                        logger.error('Error occurred: ' + err.message, 'create()');
-                                        reject(err);
-                                    } else {
-                                        resolve({ data: { createdId: result.id, createdRevId: result.rev }, statusCode: 201 });
-                                        console.log(result);
-                                    }
-                                });
-                              
+                                }).catch();
+
+
                             }
                  });
 
@@ -213,144 +231,3 @@ module.exports = {
         })
     }
 }
-*/
-async function pythonFunction (value){
-    //Here-python-worked-for-me-instead-of-python3
-  return new Promise((resolve,reject)=>{
-
-    const python = spawn('python', ['./web_plagiarism_check.py',value]);
-    // collect data from script
-    console.log("In-function-report");
-    var report;
-    python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...');
-        report = data.toString();
-        console.log(report);
-        console.log(value);
-    });
-    
-    // in close event we are sure that stream from child process is closed
-    python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-    let url =report.substring(report.indexOf("u")+1,report.indexOf(")")).replace("'",'');
-    let percent=report.substring(1,report.indexOf(","));
-    // console.log(url);
-    // console.log(percent);
-    // let list=[];
-    var plagiarismReport={
-        "percentageCopied":percent,
-        "urlUsed":url
-    }
-    // list.push(plagiarismReport)
-    // console.log(plagiarismReport);
-    resolve(plagiarismReport);
-    // res.send(report)
-    });
-    
-
-
-
-  })
-
-}
-
-
-
-////Used-to-create-Put-request
-
-let updateReport = (faceSuspicion, eyeSuspicion, tabSwitches,test_content, email, testId)=>{
-    return new Promise((resolve, reject) => {
-        db.find({
-            'selector': {
-                'type':"report",
-                'test_id':testId,
-                'student_email':email
-                  }
-                    }, (err, documents) => {
-                        if (err) {
-                            reject(err);
-                        } else if(documents.docs.length === 0){
-                            reject(err);
-                        }
-                        else {
-        
-                            // resolve({ data: JSON.stringify(documents.docs), statusCode: (documents.docs.length > 0) ? 200 : 404 });
-                            let list=documents.docs[0];
-        
-                            list.face_suspicion=faceSuspicion;
-                            list.eye_suspicion=eyeSuspicion;
-                            list.tab_switches=tabSwitches;
-
-                            list.plagiarism_report=[];
-                            var flag=0;
-                            for(var items of test_content){
-                                if(items.isSubjective){
-                                    console.log(items.answer);
-
-
-                                    const python = spawn('python', ['./web_plagiarism_check.py',items.answer]);
-    // collect data from script
-    console.log("In-function-report");
-    var report;
-    python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...');
-        report = data.toString();
-        console.log(report);
-        // console.log(value);
-    });
-    
-    // in close event we are sure that stream from child process is closed
-    python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-    let url =report.substring(report.indexOf("u")+1,report.indexOf(")")).replace("'",'');
-    let percent=report.substring(1,report.indexOf(","));
-    // console.log(url);
-    // console.log(percent);
-    // let list=[];
-    var plagiarismReport={
-        "percentageCopied":percent,
-        "urlUsed":url
-    }
-    // list.push(plagiarismReport)
-    // console.log(plagiarismReport);
-    list.plagiarism_report.push(plagiarismReport);
-    // res.send(report)
-    });
-                                    
-                                } 
-
-                                
-                                flag++;
-                            }
-                            resolve(list)
-                        }
-             });
-    })
-   
-}
-var test_content=[
-    {"isSubjective":true,
-     "question":"Multithreading",
-     "answer":"A Thread is small piece of task"
-  },{
-    "isSubjective":true,
-    "question":"overriding",
-    "answer":"When we over a method in subclass"
-  },{
-    "isSubjective":false,
-    "question":"Multithreading",
-    "answer":"a"
-  }
-]
-
-updateReport(10,20,20,test_content,'tanish.s@somaiya.edu','afb2120a-95b7-46d3-a343-9db08a9005d3').then((list)=>{
-    db.insert(list, (err, result) => {
-        if (err) {
-            logger.error('Error occurred: ' + err.message, 'create()');
-            console.log(err);
-        } else {
-            console.log({ data: { createdId: result.id, createdRevId: result.rev }, statusCode: 201 });
-            console.log(result);
-        }
-    });
-}).catch()
